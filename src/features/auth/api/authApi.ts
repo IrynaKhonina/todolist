9b17792a14c400463a3b349e8095e1e17c6ebad0
IrynaKhonina <irinahonina@yahoo.com>
2025-06-01 19:29:17 +0300

    5-2-18 2/1

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,9 +1,31 @@
 import { instance } from "@/common/instance"
 import type { BaseResponse } from "@/common/types"
 import type { LoginArgs } from "./authApi.types"
+import { baseApi } from "@/app/baseApi.ts"
 
 
+export const authApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    me: build.query<BaseResponse<{ id: number; email: string; login: string }>, void>({
+      query: () => "auth/me",
+    }),
+    login: build.mutation<BaseResponse<{ userId: number; token: string }>, LoginArgs>({
+      query: (body) => ({
+        url: "auth/login",
+        method: "POST",
+        body,
+      }),
+    }),
+    logout: build.mutation<BaseResponse, void>({
+      query: () => ({
+        url: "auth/login",
+        method: "DELETE",
+      }),
+    }),
+  }),
+})
 
+export const { useMeQuery, useLoginMutation, useLogoutMutation } = authApi
 
 export const _authApi = {
   login(payload: LoginArgs) {
