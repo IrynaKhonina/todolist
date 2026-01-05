3b99bbb8639a1f5cda404cdd79b75320184d0dff
IrynaKhonina <irinahonina@yahoo.com>
2026-01-05 20:20:58 +0300

    rell1.1.1

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -1,3 +1,31 @@
+// import { AUTH_TOKEN } from "@/common/constants"
+// import { handleError } from "@/common/utils"
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+//
+// export const baseApi = createApi({
+//   reducerPath: "todolistsApi",
+//   tagTypes: ["Todolist", "Task"],
+//   keepUnusedDataFor: 5,
+//   refetchOnFocus: true,
+//   refetchOnReconnect: true,
+//
+//   baseQuery: async (args, api, extraOptions) => {
+//     const result = await fetchBaseQuery({
+//       baseUrl: import.meta.env.VITE_BASE_URL,
+//       credentials: "include",
+//       prepareHeaders: (headers) => {
+//         headers.set("API-KEY", import.meta.env.VITE_API_KEY)
+//         headers.set("Authorization", `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
+//       },
+//     })(args, api, extraOptions)
+//
+//     handleError(api, result)
+//
+//     return result
+//   },
+//   endpoints: () => ({}),
+// })
+
 import { AUTH_TOKEN } from "@/common/constants"
 import { handleError } from "@/common/utils"
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
@@ -14,8 +42,14 @@ export const baseApi = createApi({
       baseUrl: import.meta.env.VITE_BASE_URL,
       credentials: "include",
       prepareHeaders: (headers) => {
+        const token = localStorage.getItem(AUTH_TOKEN)
+
         headers.set("API-KEY", import.meta.env.VITE_API_KEY)
-        headers.set("Authorization", `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
+
+        // Добавляем Authorization только если токен есть
+        if (token) {
+          headers.set("Authorization", `Bearer ${token}`)
+        }
       },
     })(args, api, extraOptions)
 
@@ -25,4 +59,3 @@ export const baseApi = createApi({
   },
   endpoints: () => ({}),
 })
-//got
